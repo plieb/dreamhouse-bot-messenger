5818e084fd616054538836efdc75aebc4e61cf30
PE Lieb <lieb.pe@gmail.com>
2017-01-29 17:26:24 +0100

    [ADD] converse response

--- server.js
+++ server.js
@@ -41,6 +41,23 @@ app.post('/webhook', (req, res) => myBot.listen(req, res))
 
 myBot.onTextMessage(message => {
   console.log(message)
+  const userText = message.content.attachment.content
+    const conversationToken = message.senderId
+
+	client.textConverse(userText, { conversationToken })
+	  .then(res => {
+	  // We get the first reply from Recast.AI or a default reply
+		const reply = res.reply() || 'Sorry, I didn\'t understand'
+
+		const response = {
+		  type: 'text',
+		  content: reply,
+		}
+
+		return message.reply(response)
+	  })
+	.then(() => console.log('Message successfully sent'))
+	.catch(err => console.error(`Error while sending message: ${err}`))
   })
 /*
 app.post('/webhook', (req, res) => {
