1818e283b60cd6eeb57c466fdf4646f0193bdaae
PE Lieb <lieb.pe@gmail.com>
2017-01-31 11:48:52 +0100

    [UPD] array length

--- modules/actions/houses-range.js
+++ modules/actions/houses-range.js
@@ -3,7 +3,7 @@ let salesforce = require('../salesforce'),
     messenger = require('../messenger'),
     formatter = require('../formatter');
 
-export default function cityHouses(res, replies, sender) {
+export default function cityHousesRange(res, replies, sender) {
 /*  const content = {
     title: replies[0].content,
     buttons: [{
@@ -29,9 +29,33 @@ export default function cityHouses(res, replies, sender) {
     content,
   })
   return replies*/
+  console.log('CITY HOUSES')
+
+  if (res.raw.entities.location) {
+   console.log('======================================')
+   console.log(res.raw.entities.location[0].raw)
+   console.log('======================================')
+   const city = res.raw.entities.location[0].raw
+    messenger.send({text: `OK, looking for houses in ${city}`}, sender);
+    salesforce.findProperties({city: city}).then(properties => {
+      console.log('======================================')
+      console.log(properties)
+      console.log(properties.length)
+      console.log('======================================')
+      if (properties) {
+        console.log('======================================')
+        console.log('IN PROPERTIES')
+        console.log('======================================')
+        messenger.send(formatter.formatProperties(properties), sender);
+      } else {
+        messenger.send({text: `Couldn't find any houses in ${city}`}, sender);
+      }
+    });
+  } else {
     messenger.send({text: `OK, looking for houses for sale around you...`}, sender);
     salesforce.findProperties().then(properties => {
         messenger.send(formatter.formatProperties(properties), sender);
     });
+  }
 }
 
