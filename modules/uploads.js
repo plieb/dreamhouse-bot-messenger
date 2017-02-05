29fdf0c2fbc2da38ef83077bae80a8857311aecb
PE Lieb <lieb.pe@gmail.com>
2017-02-05 01:55:05 +0100

    [FIX] attachment.content

--- modules/uploads.js
+++ modules/uploads.js
@@ -3,13 +3,12 @@ const formatter = require('./formatter')
 const salesforce = require('./salesforce')
 const visionService = require('./vision-service-mock')
 
-export default async function processUpload(attachments) {
+export default async function processUpload(attachment) {
   console.log('UPLOAD IMAGE')
-  const attachment = attachments[0]
 
   const replies = []
   replies.push(formatter.formatMsg('OK, let me look at that picture...'))
-  const houseType = await visionService.classify(attachment.url)
+  const houseType = await visionService.classify(attachment.content)
   replies.push(formatter.formatMsg(`Looking for houses matching "${houseType}"`))
   const properties = await salesforce.findPropertiesByCategory(houseType)
   if (properties.length) {
