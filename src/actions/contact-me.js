fa7c58630203f8f8f3f3bb519e026cda5661cbe4
PE Lieb <lieb.pe@gmail.com>
2017-09-10 12:48:55 +0200

    [UPD] llog data

--- src/actions/contact-me.js
+++ src/actions/contact-me.js
@@ -7,6 +7,10 @@ export default async function contactMe(res, payload, message) {
 
   const replies = []
   replies.push(formatter.formatMsg('Thanks for your interest. I asked a broker to contact you asap.'))
+  console.log('======================================')
+  console.log(message)
+  console.log(message.data)
+  console.log('======================================')
   const firstName = message.data.userName.split(' ')[0]
   const lastName = message.data.userName.split(' ')[1]
   if (payload.propertyId) {
